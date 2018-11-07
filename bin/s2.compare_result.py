c1e62f6b7fe80691e398f2dd8197f52d8cc143ed
Hao Yu <yuhao819@hotmail.com>
2018-11-07 17:23:29 +0800

    Update bin/s2.compare_result.py

--- bin/s2.compare_result.py
+++ bin/s2.compare_result.py
@@ -17,29 +17,52 @@
 
 import sys
 import os
+import matplotlib
 
 
 def __stat_fragment_directory__(parsed_result_box):
-    'This function is used for'
+    'This function is used for put the scanning result of fragment directory in order'
 
     pass
 
 
 def __stat_large_file__(parsed_result_box):
+    'This function is used for put the scanning result of large file in order'
+
+    pass
+
+
+def __stat_broken_link__(parsed_result_box):
+    'This function is used for put the scanning result of broken link in order'
+
+    pass
+
+
+def __stat_nonAccessible_directory__(parsed_result_box):
+    'This function is used for put the scanning result of non-accessible directory in order'
+
     pass
 
 
 def combine_result(result_dir_path_list):
     'This function is used for combining all the separate result into one box'
 
+    for result_dir_path in result_dir_path_list:
+        pass
+
+
+def compare_newAndOld_results(new_result_box, old_result_box):
+    'This function is used for comparing new and old results'
+
     pass
 
 
-def parse_combined_result(result_box):
-    'This function is used for parsing the combined result'
+def plot_compared_result(compared_result_box):
+    'This function is used for plotting'
 
     pass
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 3:
+        combine_result(sys.argv)
