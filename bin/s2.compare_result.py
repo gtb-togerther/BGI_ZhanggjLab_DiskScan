16272401274c9b9f4e8b0b75d7f394ab689bc2e3
Hao Yu <yuhao819@hotmail.com>
2018-11-07 14:46:05 +0800

    Rename

---
+++ bin/s2.compare_result.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+'''
+
+    Author: Hao Yu (yuhao@genomics.cn)
+    Date:   2018-11-07
+
+    ChangeLog:
+        v0.1
+            2018-11-07
+                Initiation
+
+'''
+
+
+import sys
+import os
+
+
+def __stat_fragment_directory__(parsed_result_box):
+    'This function is used for'
+
+    pass
+
+
+def __stat_large_file__(parsed_result_box):
+    pass
+
+
+def combine_result(result_dir_path_list):
+    'This function is used for combining all the separate result into one box'
+
+    pass
+
+
+def parse_combined_result(result_box):
+    'This function is used for parsing the combined result'
+
+    pass
+
+
+if __name__ == '__main__':
+    pass
