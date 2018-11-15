2b8eb7d36c37e66070d8dd76128706fce69ad87c
Hao Yu <yuhao819@hotmail.com>
2018-11-16 00:06:53 +0800

    Function Fix-up

--- bin/s2.compare_result.py
+++ bin/s2.compare_result.py
@@ -11,13 +11,15 @@
         v0.1
             2018-11-07
                 Initiation
+        v0.2
+            2018-11-15
+                To use a new data structure
 
 '''
 
 
 import sys
 import os
-import matplotlib
 
 
 def combine_result(input_list):
@@ -32,7 +34,7 @@ def combine_result(input_list):
         else:
             result_path_list.append(i)
 
-    result_box = {'FD':{},'LF':{},'BL':{},'nAD':{}}
+    result_box = {'FD':{},'LF':{},'BL':[],'nAD':{}}
 
     for result_path in result_path_list:
         with open(result_path, 'rb') as fh:
@@ -43,73 +45,90 @@ def combine_result(input_list):
                     continue
 
                 record_class = l[0]
-                record_owner = l[1]
 
                 if record_class == 'FD' or record_class == 'LF':
+                    record_owner = l[1]
                     record_inode = l[2]
+                    record_path = l[6]
+
                     record_size = l[3]
                     record_mtime = l[4]
                     record_atime = l[5]
-                    record_path = l[6]
 
                     if not result_box[record_class].has_key(record_owner):
-                        result_box[record_class].update({record_owner:[]})
-                        result_box[record_class][record_owner].append([record_inode,record_size,record_mtime,record_atime,record_path])
-                    else:
-                        result_box[record_class][record_owner].append([record_inode,record_size,record_mtime,record_atime,record_path])
+                        result_box[record_class].update({record_owner:{}})
+
+                    if not result_box[record_class][record_owner].has_key(record_inode):
+                        result_box[record_class][record_owner].update({record_inode:[record_size,record_mtime,record_atime,record_path]})
                 elif record_class == 'BL':
-                    record_path = l[2]
+                    record_path = l[1]
 
-                    if not result_box[record_class].has_key(record_owner):
-                        result_box[record_class].update({record_owner:[]})
-
-                    result_box[record_class][record_owner].append([record_path,])
+                    result_box[record_class].append(record_path)
                 elif record_class == 'nAD':
+                    record_owner = l[1]
                     record_inode = l[2]
                     record_path = l[3]
 
                     if not result_box[record_class].has_key(record_owner):
-                        result_box[record_class].update({record_owner:[]})
+                        result_box[record_class].update({record_owner:{}})
 
-                    result_box[record_class][record_owner].append([record_inode,record_path])
+                    if not result_box[record_class][record_owner].has_key(record_inode):
+                        result_box[record_class][record_owner].update({record_inode:[record_path]})
                 else:
                     print >> sys.stderr, result_path + ' may be not your kind of record in disk-scanning'
 
     return result_box
 
 
-def compare_newAndOld_results(new_result_box, old_result_box):
+def compare_newAndOld_results(new_result_box, old_result_box = None):
     'This function is used for comparing new and old results'
 
     noHandle_item_box = {}
 
     for record_class in new_result_box:
-        for record_owner in new_result_box[record_class]:
-            if record_class != 'BL':
-                if old_result_box[record_class][record_owner]:
-                    if new_result_box[record_class][record_owner][0] == old_result_box[record_class][record_owner][0]:  # error
-                        if not noHandle_item_box.has_key(record_class):
-                            noHandle_item_box.update({record_class:{}})
-                        else:
-                            if not noHandle_item_box[record_class].has_key(record_owner):
-                                noHandle_item_box[record_class].update({record_owner:[]})
-                            else:
-                                noHandle_item_box[record_class][record_owner].append([old_result_box[record_class][record_owner],new_result_box[record_class][record_owner]])
+        if not noHandle_item_box.has_key(record_class):
+            noHandle_item_box.update({record_class:{}})
+
+        if record_class != 'BL':
+            for record_owner in new_result_box[record_class]:
+                if not noHandle_item_box[record_class].has_key(record_owner):
+                    noHandle_item_box[record_class].update({record_owner:[]})
+
+                for record_inode in new_result_box[record_class][record_owner]:
+                    if old_result_box and old_result_box[record_class][record_owner][record_inode]:
+                        noHandle_item_box[record_class][record_owner].append([record_class,record_owner,\
+                                                                              new_result_box[record_class][record_owner][record_inode],\
+                                                                              old_result_box[record_class][record_owner][record_inode],'UH'])
+                    else:
+                        noHandle_item_box[record_class][record_owner].append([record_class,record_owner,\
+                                                                              new_result_box[record_class][record_owner][record_inode],['-'],'NR'])
+        else:
+            for broken_link in new_result_box[record_class]:
+                if not noHandle_item_box[record_class].has_key('WARNING'):
+                    noHandle_item_box[record_class].update({'WARNING':[]})
+
+                noHandle_item_box[record_class]['WARNING'].append([record_class,'WARNING',\
+                                                                  [broken_link],['-'],'WA'])
 
     return noHandle_item_box
 
 
-def plot_compared_result(compared_result_box):
+def report_result(compared_result_box):
     'This function is used for plotting'
 
     pass
 
 
 if __name__ == '__main__':
-    if sys.argv > 1:
-        outbox = compare_newAndOld_results(combine_result([sys.argv[1],]), combine_result([sys.argv[2],]))
+    try:
+        if len(sys.argv) == 3:
+            outbox = compare_newAndOld_results(combine_result([sys.argv[1],]), combine_result([sys.argv[2],]))
+        else:
+            outbox = compare_newAndOld_results(combine_result([sys.argv[1],]))
 
         for record_class in outbox:
             for record_owner in outbox[record_class]:
-                #print '\t'.join(outbox[record_class][record_owner])
-                pass
+                for i in outbox[record_class][record_owner]:
+                    print '\t'.join([i[0],i[1],'\t'.join(i[2]),'\t'.join(i[3]),i[-1]])
+    except:
+        print >> sys.stderr, 'USAGE:  ' + sys.argv[0] + ' <scanning outdir> [old scanning outdir]'
