9845385e5addf0be7797a42784522249db3c9ad2
Hao Yu <yuhao819@hotmail.com>
2018-12-10 11:47:23 +0800

    Function adding

--- bin/s2.compare_result.py
+++ bin/s2.compare_result.py
@@ -14,6 +14,9 @@
         v0.2
             2018-11-15
                 To use a new data structure
+        v1.0
+            2018-11-15
+                To add report function
 
 '''
 
@@ -28,9 +31,12 @@ def combine_result(input_list):
     result_path_list = []
 
     for i in input_list:
+
         if os.path.isdir(i):
+
             for r in os.listdir(i):
                 result_path_list.append(os.path.join(i, r))
+
         else:
             result_path_list.append(i)
 
@@ -119,19 +125,86 @@ def compare_newAndOld_results(new_result_box, old_result_box = None):
 def report_result(compared_result_box):
     'This function is used for plotting'
 
-    pass
+    statisticsByOwner_box = {}
+
+    for record_class in compared_result_box:
+        for record_owner in compared_result_box[record_class]:
+
+            if not statisticsByOwner_box.has_key(record_owner):
+                statisticsByOwner_box.update({record_owner:{}})
+
+            if not statisticsByOwner_box[record_owner].has_key(record_class):
+                statisticsByOwner_box[record_owner].update({record_class:{'new':0,'old':0}})
+
+            for record in compared_result_box[record_class][record_owner]:
+
+                new_item_size = 0
+                old_item_size = 0
+
+                if record_class == 'LF':
+
+                    new_item_size = float(record[2][0][:-1])
+
+                    if record[3][0] != '-':
+                        old_item_size = float(record[3][0][:-1])
+                    else:
+                        old_item_size = 0
+
+                elif record_class == 'FD':
+
+                    new_item_size = int(record[2][0])
+
+                    if record[3][0] != '-':
+                        old_item_size = int(record[3][0])
+                    else:
+                        old_item_size = 0
+
+                elif record_class == 'nAD':
+
+                    new_item_size = 1
+
+                    if record[3][0] != '-':
+                        old_item_size = 1
+                    else:
+                        old_item_size = 0
+
+                statisticsByOwner_box[record_owner][record_class]['new'] += new_item_size
+                statisticsByOwner_box[record_owner][record_class]['old'] += old_item_size
+
+    return statisticsByOwner_box
 
 
 if __name__ == '__main__':
+
+    fh_output = open(sys.argv[1] + '.output.txt', 'wb')
+    fh_report = open(sys.argv[1] + '.report.txt', 'wb')
+
     try:
-        if len(sys.argv) == 3:
-            outbox = compare_newAndOld_results(combine_result([sys.argv[1],]), combine_result([sys.argv[2],]))
+        if len(sys.argv) == 4:
+            outbox = compare_newAndOld_results(combine_result([sys.argv[2],]), combine_result([sys.argv[3],]))
         else:
-            outbox = compare_newAndOld_results(combine_result([sys.argv[1],]))
+            outbox = compare_newAndOld_results(combine_result([sys.argv[2],]))
 
         for record_class in outbox:
             for record_owner in outbox[record_class]:
-                for i in outbox[record_class][record_owner]:
-                    print '\t'.join([i[0],i[1],'\t'.join(i[2]),'\t'.join(i[3]),i[-1]])
+                for record in outbox[record_class][record_owner]:
+                    print >> fh_output, '\t'.join([record[0], record[1], '\t'.join(record[2]), '\t'.join(record[3]), record[-1]])
+
+        report_box = report_result(outbox)
+
+        for report_owner in sorted(report_box.keys()):
+            for report_class in sorted(report_box[report_owner].keys()):
+
+                consumed_ration = '-'
+
+                if report_box[report_owner][report_class]['old'] > 0:
+                    consumed_ration = '%.2f' % (report_box[report_owner][report_class]['new'] / report_box[report_owner][report_class]['old'] * 100)
+
+                if report_class == 'LF':
+                    print >> fh_report, report_owner, '\t', report_class + ': ', str(report_box[report_owner][report_class]['new']) + 'G', '/', \
+                                                                                 str(report_box[report_owner][report_class]['old']) + 'G', '\t', consumed_ration
+                else:
+                    print >> fh_report, report_owner, '\t', report_class + ': ', str(report_box[report_owner][report_class]['new']), '/', \
+                                                                                 str(report_box[record_owner][record_class]['old']), '\t', consumed_ration
     except:
-        print >> sys.stderr, 'USAGE:  ' + sys.argv[0] + ' <scanning outdir> [old scanning outdir]'
+        print >> sys.stderr, 'USAGE:  ' + sys.argv[0] + ' <prefix> <scanning outdir> [old scanning outdir]'
